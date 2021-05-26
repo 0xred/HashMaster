a254d93bc78b41cdb9395f0fb040e3f7875c2096
0xredshadow <alblowy1@gmail.com>
2021-05-26 15:22:44 +0300

    Update and rename hashmap.py to hashmaster.py

--- hashmaster.py
+++ hashmaster.py
@@ -11,7 +11,7 @@ from sys import argv, exit
 #############################################
 os.system('cls' if os.name == 'nt' else 'clear')
 os.system("color B")
-ascii_banner = pyfiglet.figlet_format("Hash Map")
+ascii_banner = pyfiglet.figlet_format("Hash Master")
 print(ascii_banner)
 ################### Menu ####################
 def menu():
